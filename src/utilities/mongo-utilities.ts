47934aa18f390e072c329823fc91573851ab39c2
brad <bradleylignoski@gmail.com>
2024-02-02 22:07:19 -0800

    temp hard-code mongo uri to debug circleci config

--- src/utilities/mongo-utilities.ts
+++ src/utilities/mongo-utilities.ts
@@ -1,18 +1,19 @@
   const envType = process.env.ENV_TYPE || 'local';
   const dbName = 'myAppDb';
 
-export const getMongoUri = () => {
-  switch (envType) {
-    case 'local':
-      return `mongodb://localhost:27017/${dbName}`;
-    case 'docker':
-      return `mongodb://mongo:27017/${dbName}`;
-    case 'circleci':
-      // Add CircleCI specific URI if needed
-      return `mongodb://mongo:27017/${dbName}`;
-    // Add more cases as needed
-    default:
-      return `mongodb://localhost:27017/${dbName}`;
-  }
-};
+// export const getMongoUri = () => {
+//   switch (envType) {
+//     case 'local':
+//       return `mongodb://localhost:27017/${dbName}`;
+//     case 'docker':
+//       return `mongodb://mongo:27017/${dbName}`;
+//     case 'circleci':
+//       // Add CircleCI specific URI if needed
+//       return `mongodb://mongo:27017/${dbName}`;
+//     // Add more cases as needed
+//     default:
+//       return `mongodb://localhost:27017/${dbName}`;
+//   }
+// };
 
+export const getMongoUri = () => `mongodb://mongo:27017/${dbName}`;
